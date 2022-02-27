5fa1c2ce796e430178cb710d700c09fa806f25a9
Max Lynch <max@ionic.io>
2022-02-27 11:32:21 -0600

    Add watch plugin to Android

--- android/app/src/main/java/io/ionic/demoapp/ionifits/MainActivity.java
+++ android/app/src/main/java/io/ionic/demoapp/ionifits/MainActivity.java
@@ -8,4 +8,9 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(WatchPlugin.class);
+  }
 }
---
+++ android/app/src/main/java/io/ionic/demoapp/ionifits/WatchPlugin.java
@@ -0,0 +1,11 @@
+package io.ionic.demoapp.ionifits;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "Watch")
+public class WatchPlugin extends Plugin {
+}
